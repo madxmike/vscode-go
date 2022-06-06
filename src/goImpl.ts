4d09baa0c8e4e73d95a8b06713a5f9d02475d821
Michael Epps <mep053@gmail.com>
2022-06-05 23:56:50 -0400

    Reimplement interface stub command to provide a better ui experience

--- src/goImpl.ts
+++ src/goImpl.ts
@@ -7,70 +7,121 @@
 
 'use strict';
 
-import cp = require('child_process');
-import { dirname } from 'path';
-import { toolExecutionEnvironment } from './goEnv';
-import { promptForMissingTool } from './goInstallTools';
-import { getBinPath } from './util';
 import vscode = require('vscode');
 import { CommandFactory } from './commands';
+import { debounce } from 'lodash';
+import { ExecuteCommandParams, ExecuteCommandRequest, integer, SymbolKind } from 'vscode-languageserver-protocol';
+import { GoExtensionContext } from './context';
 
-// Supports only passing interface, see TODO in implCursor to finish
-const inputRegex = /^(\w+\ \*?\w+\ )?([\w\.\-\/]+)$/;
 
-export const implCursor: CommandFactory = () => () => {
+class SymbolItem implements vscode.QuickPickItem {
+	public label: string;
+	public description: string;
+	public name: string;
+	public package: string;
+	public location: vscode.Location;
+	constructor(symbol: vscode.SymbolInformation) {
+		const kindName = vscode.SymbolKind[symbol.kind].toLowerCase();
+		this.label = `$(symbol-${kindName}) ${symbol.name}`;
+		this.description = symbol.containerName;
+		this.name = symbol.name.split('.').pop(); // in case, symbol contains package name.
+		this.package = symbol.containerName;
+		this.location = symbol.location;
+	}
+}
+
+
+
+export const implCursor: CommandFactory = (ctx, goCtx) => async () => {
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) {
-		vscode.window.showErrorMessage('No active editor found.');
+		vscode.window.showErrorMessage('No active editor found to implement interfaces.');
 		return;
 	}
-	const cursor = editor.selection;
-	return vscode.window
-		.showInputBox({
-			placeHolder: 'f *File io.Closer',
-			prompt: 'Enter receiver and interface to implement.'
-		})
-		.then((implInput) => {
-			if (typeof implInput === 'undefined') {
+
+	const cursorPosition = editor.selection.start;
+	const wordRange = editor.document.getWordRangeAtPosition(cursorPosition);
+	const selectedText = wordRange ? editor.document.getText(wordRange) : '';
+
+	// TODO: Type with underlying types have different vscode.SymbolKinds than structs. 
+	// Unfortunately, they also share those vscode.SymbolKinds with non-type symbols.
+	// E.g. both type Foo func() and func foo() {} are a vscode.SymbolKind.Function.
+	// We cannot just implement this as a snippet and levergage stubbing in gopls currently 
+	// as gopls needs the concrete type to stub. 
+
+	// TODO: It only makes sense to being able to find symbols that are within the same package
+	const structItem = await quickPickSymbol("Input struct name (e.g. Client)", selectedText, [vscode.SymbolKind.Struct, vscode.SymbolKind.Class]);
+	const interfaceItem = await quickPickSymbol("Input inteface name (e.g. Client)", '', [vscode.SymbolKind.Interface]);
+
+	generateInterfaceStubs(goCtx, editor, structItem, interfaceItem);
+}
+
+async function quickPickSymbol(placeholder: string, initialValue: string, allowedKinds: vscode.SymbolKind[]): Promise<SymbolItem> {
+	return new Promise((resolve, reject) => {
+		const quickPick = vscode.window.createQuickPick()
+		quickPick.placeholder = placeholder
+		quickPick.canSelectMany = false;
+		quickPick.value = initialValue;
+	
+		const search = async function (keyword: string) {
+			quickPick.busy = true;
+			const symbols = await vscode.commands
+				.executeCommand<vscode.SymbolInformation[]>('vscode.executeWorkspaceSymbolProvider', keyword);
+			
+			const items = symbols
+				.filter(s => allowedKinds.includes(s.kind))
+				.map(s => new SymbolItem(s))
+
+			quickPick.items = items;
+			quickPick.busy = false;
+		};
+
+		// Note: Immediately populate the results to provide a snappier experience.
+		search(quickPick.value);
+	
+		quickPick.onDidChangeValue(debounce(search, 250));
+		quickPick.onDidChangeSelection((selections: readonly vscode.QuickPickItem[]) => {
+			if (typeof selections === 'undefined') {
 				return;
 			}
-			const matches = implInput.match(inputRegex);
-			if (!matches) {
-				vscode.window.showInformationMessage(`Not parsable input: ${implInput}`);
-				return;
+			
+			const item = selections[0];
+			if (item instanceof SymbolItem) {
+				resolve(item);
+				quickPick.dispose();
 			}
-
-			// TODO: automatically detect type name at cursor
-			// if matches[1] is undefined then detect receiver type
-			// take first character and use as receiver name
-
-			runGoImpl([matches[1], matches[2]], cursor.start, editor);
 		});
-};
+		quickPick.show();
+	})
+}
 
-function runGoImpl(args: string[], insertPos: vscode.Position, editor: vscode.TextEditor) {
-	const goimpl = getBinPath('impl');
-	const p = cp.execFile(
-		goimpl,
-		args,
-		{ env: toolExecutionEnvironment(), cwd: dirname(editor.document.fileName) },
-		(err, stdout, stderr) => {
-			if (err && (<any>err).code === 'ENOENT') {
-				promptForMissingTool('impl');
-				return;
-			}
 
-			if (err) {
-				vscode.window.showInformationMessage(`Cannot stub interface: ${stderr}`);
-				return;
-			}
+async function generateInterfaceStubs(goCtx: GoExtensionContext, editor: vscode.TextEditor, structItem: SymbolItem, interfaceItem: SymbolItem): Promise<void> {
+	const { languageClient, serverInfo } = goCtx;
+	const COMMAND = 'gopls.generate_interface_stubs';
 
-			editor.edit((editBuilder) => {
-				editBuilder.insert(insertPos, stdout);
-			});
+	if (languageClient && serverInfo?.Commands?.includes(COMMAND)) {
+		try {
+			const uri = languageClient.code2ProtocolConverter.asTextDocumentIdentifier(editor.document).uri;
+			const concreteLocation = languageClient.code2ProtocolConverter.asLocation(structItem.location);
+			const interfaceLocation = languageClient.code2ProtocolConverter.asLocation(interfaceItem.location);
+			const params: ExecuteCommandParams = {
+				command: COMMAND,
+				arguments: [
+					{
+						URI: uri,
+						ConcreteLocation: concreteLocation,
+						InterfaceLocation: interfaceLocation
+					}
+				]
+			};
+
+			console.log(JSON.stringify(params.arguments));
+			const resp = await languageClient.sendRequest(ExecuteCommandRequest.type, params);
+			return resp.Packages;
+		} catch (e) {
+			vscode.window.showErrorMessage('Failed to generate interface method stubs.');
+			console.log(`error with ${COMMAND}: ${e}`);
 		}
-	);
-	if (p.pid) {
-		p.stdin?.end();
 	}
 }
